3ba5b4bfe71bc735f02de509f7f2446f5d3bba2d
Jorge A Jimenez <57850031+Caleno83@users.noreply.github.com>
2020-11-15 13:28:42 -0500

    Revert "Revert "Revert "added test for index, customers, sellers, and items endpoints"""

--- api/server.js
+++ api/server.js
@@ -25,11 +25,8 @@ server.use((err, req, res, next) => {
   });
 });
 
-
 server.get("/", (req, res) => {
-	res.json({
-		message: "Welcome to Build Week Project - African Marketplace",
-	})
-})
+  res.send(`<h2>Welcome to Build Week Project - African Marketplace</h2>`);
+});
 
 module.exports = server;
