bf90e758488abdb47503d341242f4da395a0b350
Jorge A Jimenez <57850031+Caleno83@users.noreply.github.com>
2020-11-15 13:13:55 -0500

    Revert "added test for index, customers, sellers, and items endpoints"

--- __tests__/index.js
+++
@@ -1,9 +0,0 @@
-const supertest = require("supertest")
-const server = require("../api/server")
-
-test("GET message introduction test", async () => {
-	const res = await supertest(server).get("/")
-	expect(res.statusCode).toBe(200)
-	expect(res.type).toBe("application/json")
-	expect(res.body.message).toBe("Welcome to Build Week Project - African Marketplace")
-})
--- knexfile.js
+++ knexfile.js
@@ -20,19 +20,6 @@ module.exports = {
 			},
 		},
 	},
-	testing: {
-		client: "sqlite3",
-		useNullAsDefault: true,
-		connection: {
-			filename: "./data/test.db3",
-		},
-		migrations: {
-			directory: "./data/migrations",
-		},
-		seeds: {
-			directory: "./data/seeds",
-		},
-	},
 	production: {
         client: "pg",
         connection: process.env.DATABASE_URL,
